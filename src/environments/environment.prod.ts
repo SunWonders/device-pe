b3483f10dd44d078d9fec64a58b150cc782a683a
Dilip Duraiswamy <dilipduraiswamy@gmail.com>
2021-05-16 15:01:55 +0530

    adding missed urls in prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:4200',
   brandUrl:'http://34.70.99.100:6090/devicePe-ws/v1/brands/find/all',
-  brandSaveUrl:'http://34.70.99.100:6090/devicePe-ws/v1/brands/save'
+  brandSaveUrl:'http://34.70.99.100:6090/devicePe-ws/v1/brands/save',
+  brandUpdateUrl:'http://34.70.99.100:6090/devicePe-ws/v1/brands/update'
 };
