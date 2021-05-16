58ac8f161e03f2d64959a27c6e82840d0d72324a
Dilip Duraiswamy <dilipduraiswamy@gmail.com>
2021-05-16 18:10:30 +0530

    missed prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   brandSaveUrl:'http://34.70.99.100:6090/devicePe-ws/v1/brands/save',
   brandUpdateUrl:'http://34.70.99.100:6090/devicePe-ws/v1/brands/update',
   accessoriesUrl:'http://34.70.99.100:6090/devicePe-ws/v1/accessories/find/all',
-  accessoriesUpdate:'http://34.70.99.100:6090/devicePe-ws/v1/accessories/update',
+  accessoriesUpdateUrl:'http://34.70.99.100:6090/devicePe-ws/v1/accessories/update',
   modelsUrl:'http://34.70.99.100:6090/devicePe-ws/v1/models/find/all',
   modelsUpdateUrl:'http://34.70.99.100:6090/devicePe-ws/v1/models/update'
 };
